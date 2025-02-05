cc2afaefe9be4f6234a162131df54e5279a7a40b
Arindam Deb <Arindamdeb2000@icloud.com>
2025-02-05 14:35:17 +0530

    Update Jarvis.js

--- Jarvis.js
+++ Jarvis.js
@@ -2,7 +2,7 @@
 </head>
 <body>
     <div class="chat-container">
-        <img src="YOUR_UPLOADED_IMAGE_PATH" class="avatar" alt="ARI's Avatar">
+        <img src="1728714034242 (1).jpeg" class="avatar" alt="ARI's Avatar">
         <h2>ARI's Jarvis</h2>
         <div class="chat-box" id="chatBox">Welcome to ARI's Jarvis!</div>
         <input type="text" id="userInput" class="user-input" placeholder="Type your message...">
